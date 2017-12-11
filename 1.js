22479cf647f1a28f23bf3dc2c7c552b07a7f38bf
qiyutao <qiyutao12@163.com>
2017-12-11 20:13:52 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,6 @@
 (function(ext) {
+    var alarm_went_off = false; // This becomes true after the alarm goes off
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -8,12 +10,31 @@
         return {status: 2, msg: 'Ready'};
     };
 
+    ext.set_alarm = function(time) {
+       window.setTimeout(function() {
+           alarm_went_off = true;
+       }, time*1000);
+    };
+
+    ext.when_alarm = function() {
+       // Reset alarm_went_off if it is true, and return true
+       // otherwise, return false.
+       if (alarm_went_off === true) {
+           alarm_went_off = false;
+           return true;
+       }
+
+       return false;
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
+            ['', 'run alarm after %n seconds', 'set_alarm', '2'],
+            ['h', 'when alarm goes off', 'when_alarm'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('Alarm extension', descriptor, ext);
 })({});
